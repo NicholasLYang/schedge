870380db74e729f4bf3964058ebf9ac66f13a488
Nicholas Yang <ny585@nyu.edu>
2020-06-02 23:57:28 -0400

    Updating docker to work

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM gradle:jdk13 AS build
 COPY --chown=gradle:gradle . /home/gradle/schedge
 WORKDIR /home/gradle/schedge
 
-RUN gradle build --no-daemon
-
 EXPOSE 8080
 
 ENTRYPOINT ["./docker-entry.sh"]
--- docker-entry.sh
+++ docker-entry.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+./gradlew updateDb
+gradle build --no-daemon
+
 java -XX:+UseContainerSupport -Djava.security.egd=file:/dev/./urandom \
   -jar /home/gradle/schedge/.build/libs/schedge.jar db serve
